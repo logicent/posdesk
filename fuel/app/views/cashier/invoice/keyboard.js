86edafe0c6a071d3111bc96eba5e2c501b767a7d
Ken Mwai <mwaigichuhi@gmail.com>
2020-07-14 10:04:19 +0300

    Added jquery.key.js for Keyboard shortcuts to be implemented

---
+++ fuel/app/views/cashier/invoice/keyboard.js
@@ -0,0 +1,16 @@
+// Keyboard Shortcuts
+// F1 - Help
+// F2 - (edit Input)
+// F3 - Find or Scan
+// F4 - Lock / Unlock
+// F5 - Reload (Refresh)
+// F6 - Switch Panel (Items/Summary or List/Grid)
+// F7 - Sales Return
+// F8 - Sales Reports
+// F9 - Hold / Continue
+// F10 - Pay Now or Pay Later
+// F11 - Minimal screen (Cashier mode)
+// F12 - 
+// End - 
+// Home - Cashier, Storekeeper or Dashboard
+// Esc - Cancel sale
